ad00e5c8b0dd3acb304270c82feabd466d2764fc
Danny <dannydotdev@gmail.com>
2025-05-23 10:46:35 +0100

    File-structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Book from "@/components/Book";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Reason from "@/components/Reason";
 import Reviews from "@/components/Reviews";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
@@ -12,6 +14,8 @@ export default function Home() {
       <Reason />
       <Reviews />
       <Book />
+      <Services />
+      <Footer />
     </div>
   );
 }
