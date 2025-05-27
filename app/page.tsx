1affc79d5eaaa4d2a04478c2a93b5e1d46d0ec6a
Danny <dannydotdev@gmail.com>
2025-05-27 01:46:00 +0100

    build-tomoroow

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Reason from "@/components/Reason";
 import Reviews from "@/components/Reviews";
@@ -11,14 +9,12 @@ import About from "@/components/About";
 export default function Home() {
   return (
     <div>
-      {/* <Header /> */}
       <Hero />
       <Reason />
       <Services />
       <Projects />
       <Reviews />
       <About />
-      {/* <Footer /> */}
       <ScrollToTopButton />
     </div>
   );
