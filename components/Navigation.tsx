dbd31270809e05eb805ff4408b70f31ab7ced0e0
Danny <dannydotdev@gmail.com>
2025-08-20 21:35:42 +0100

    Fixed unused variable

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,8 +1,6 @@
 "use client";
-
 import React, { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { Button } from "./ui/button";
 import { Menu, X } from "lucide-react"; // Import X icon
 
